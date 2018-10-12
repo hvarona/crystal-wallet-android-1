074e37076de2aae5fe996d1729f093e781513127
dtvv <coritocorito@hotmail.com>
2018-10-12 03:04:07 -0500

    -Add the new buttons push animation to the buttons in the activity_licence.xml and java source

--- app/src/main/java/cy/agorise/crystalwallet/activities/LicenseActivity.java
+++ app/src/main/java/cy/agorise/crystalwallet/activities/LicenseActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+
+import com.thekhaeng.pushdownanim.PushDownAnim;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,7 +24,16 @@ public class LicenseActivity extends AppCompatActivity {
 
     @BindView(R.id.wvEULA) WebView wvEULA;
 
+    @BindView(R.id.btnDisAgree)
+    Button btnDisAgree;
+
+    @BindView(R.id.btnAgree)
+    Button btnAgree;
+
     CrystalDatabase db;
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +41,24 @@ public class LicenseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_license);
 
         ButterKnife.bind(this);
+
+        /*
+         *   Integration of library with button efects
+         * */
+        PushDownAnim.setPushDownAnimTo(btnDisAgree)
+                .setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public void onClick( View view ){
+                        onDisagree();
+                    }
+                } );
+        PushDownAnim.setPushDownAnimTo(btnAgree)
+                .setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public void onClick( View view ){
+                        onAgree();
+                    }
+                } );
 
         db = CrystalDatabase.getAppDatabase(this.getApplicationContext());
         int licenseVersion = getResources().getInteger(R.integer.license_version);
