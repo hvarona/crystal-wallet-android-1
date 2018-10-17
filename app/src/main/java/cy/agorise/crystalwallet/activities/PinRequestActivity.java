3941585740e9e30f13901d7b0a49da52238fc5e6
dtvv <coritocorito@hotmail.com>
2018-10-17 04:14:43 -0500

    When the PIN is typed 4 wrong times, it has to block 15 seconds before make new tries

--- app/src/main/java/cy/agorise/crystalwallet/activities/PinRequestActivity.java
+++ app/src/main/java/cy/agorise/crystalwallet/activities/PinRequestActivity.java
@@ -12,6 +12,7 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
@@ -40,6 +41,19 @@ public class PinRequestActivity extends AppCompatActivity {
 
     @BindView(R.id.btnOK)
     Button btnOK;
+
+    @BindView(R.id.txtBadtry)
+    TextView txtBadtry;
+
+    /*
+    * Contains the bad tries
+    * */
+    private int tries = 0;
+
+    /*
+    * Seconds counter
+    * */
+    private int seconds = 15;
 
 
 
@@ -98,10 +112,73 @@ public class PinRequestActivity extends AppCompatActivity {
         else{
 
             /*
+            * One more bad try
+            * */
+            ++tries;
+
+            final Activity activity = this;
+
+            etPassword.setTextColor(Color.RED);
+
+            /*
+            * User can not go more up to 5 bad tries
+            * */
+            if(tries==4){
+                tries = 0;
+
+                btnOK.setEnabled(false);
+                etPassword.setEnabled(false);
+                txtBadtry.setVisibility(View.VISIBLE);
+
+                txtBadtry.setText(txtBadtry.getText().toString().replace("%%",String.valueOf(seconds)));
+
+                final Timer t = new Timer();
+                //Set the schedule function and rate
+                t.scheduleAtFixedRate(new TimerTask() {
+
+                                          @Override
+                                          public void run() {
+
+                                              --seconds;
+
+                                              if(seconds==0){
+                                                  t.cancel();
+
+                                                  seconds = 15;
+
+                                                  activity.runOnUiThread(new Runnable() {
+                                                      @Override
+                                                      public void run() {
+                                                          btnOK.setEnabled(true);
+                                                          etPassword.setEnabled(true);
+                                                          txtBadtry.setVisibility(View.INVISIBLE);
+                                                          etPassword.setText("");
+                                                          etPassword.setTextColor(Color.WHITE);
+                                                      }
+                                                  });
+                                              }
+                                              else{
+                                                  activity.runOnUiThread(new Runnable() {
+                                                      @Override
+                                                      public void run() {
+                                                          txtBadtry.setText(activity.getResources().getString(R.string.wrong_pin_wait).replace("%%",String.valueOf(seconds)));
+                                                      }
+                                                  });
+                                              }
+                                          }
+
+                                      },
+                        //Set how long before to start calling the TimerTask (in milliseconds)
+                        1000,
+                        //Set the amount of time between each execution (in milliseconds)
+                        1000);
+
+                return;
+            }
+
+            /*
             *   Set in red the rext and reset the password after a period of time
             * */
-            final Activity activity = this;
-            etPassword.setTextColor(Color.RED);
             final Timer t = new Timer();
             //Set the schedule function and rate
             t.scheduleAtFixedRate(new TimerTask() {
